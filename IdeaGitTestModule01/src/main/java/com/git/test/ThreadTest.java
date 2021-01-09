6c79351a78711f840e628ac0528614fba10d6aef
ZhangYi959024 <create_zhangyi@163.com>
2021-01-09 19:00:51 +0800

    修改了ThreadTest.java

--- IdeaGitTestModule01/src/main/java/com/git/test/ThreadTest.java
+++ IdeaGitTestModule01/src/main/java/com/git/test/ThreadTest.java
@@ -2,12 +2,14 @@ package com.git.test;
 
 public class ThreadTest {
     public static void main(String[] args) {
+        new Thread(()-> System.out.println("1.8特性1")).start();
+
         new Thread(()-> {
             for (int i = 0; i < 500; i++) {
                 System.out.println(i);
             }
         }).start();
-        new Thread(()-> System.out.println("1.8特性")).start();
+
     }
 
 }
