e79b6fe73d5303cadf0debee044a9d846a594067
ZhangYi959024 <create_zhangyi@163.com>
2021-01-09 16:58:51 +0800

    ssh 免密登录测试 insert文件

---
+++ SSHTest.java
@@ -0,0 +1,12 @@
+package com.test.thread.callable;
+
+public class Demo{
+
+
+    public void test(){
+        System.out.println("clone insert");
+    }
+	public void login(){
+		System.out.println("ssh 免密登录设置完成");
+	}
+}
