43e36ba9de432c22877799d16c47af52f01b172b
ZhangYi959024 <create_zhangyi@163.com>
2021-01-09 17:29:53 +0800

    新增GitTest.java 增加属性 name  flag

---
+++ IdeaGitTestModule01/src/main/java/com/git/test/GitTest.java
@@ -0,0 +1,14 @@
+package com.git.test;
+
+public class GitTest {
+    public String name;
+    public boolean flag;
+
+    public GitTest() {
+    }
+
+    public GitTest(String name, boolean flag) {
+        this.name = name;
+        this.flag = flag;
+    }
+}
