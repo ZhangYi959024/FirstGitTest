631f4422cabce4cbec880d88ae757ef3b2c84e26
ZhangYi959024 <create_zhangyi@163.com>
2021-01-09 18:03:08 +0800

    增加ThreadTest.java

---
+++ IdeaGitTestModule01/src/main/java/com/git/test/ThreadTest.java
@@ -0,0 +1,13 @@
+package com.git.test;
+
+public class ThreadTest {
+    public static void main(String[] args) {
+        new Thread(()-> {
+            for (int i = 0; i < 500; i++) {
+                System.out.println(i);
+            }
+        }).start();
+        new Thread(()-> System.out.println("1.8特性")).start();
+    }
+
+}
